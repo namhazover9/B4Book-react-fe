96051a753b02b7741d72a14085fffbc0139eccde
Minh Trí Trần <tritranrpc@gmail.com>
2024-11-14 14:17:21 +0700

    move language

--- src/layouts/Layout.jsx
+++ src/layouts/Layout.jsx
@@ -1,11 +1,34 @@
-import React from 'react'
-
+import React from 'react';
+import { Select } from 'antd';
+import { languages } from '../constants/constants';
+import Translate from '../components/Common/Translate';
+import { useLocalization } from '../context/LocalizationWrapper';
 export default function Layout({ children }) {
+
+    const { switchLocale } = useLocalization();
+
+    const handleChange = (value) => {
+        switchLocale(value);
+    };
 
     return (
         <div className='m-4 font-cairoRegular'>
             <header className='flex items-center justify-center gap-x-10 bg-DeepNightBlack text-Snow w-full h-14 rounded-xl'>
                 <h1 className='text-xl text-Green font-semibold'>React - Vite - Starter</h1>
+                <div className="flex items-center justify-center gap-y-4 flex-col mt-16">
+                <h2 className='text-3xl text-white font-semibold'>
+                    <Translate text='Localization Setup Integrated' />
+                </h2>
+                <Select
+                    defaultValue={localStorage.getItem('locale') ?? 'en'}
+                    className='h-16 w-20'
+                    style={{
+                        width: '200px'
+                    }}
+                    onChange={handleChange}
+                    options={languages}
+                />
+            </div>
             </header>
             {children}
             {/* <footer></footer> */}
--- src/pages/homepages/Home.jsx
+++ src/pages/homepages/Home.jsx
@@ -14,36 +14,10 @@ export default function Home() {
 
     return (
         <div className='flex flex-col gap-y-4'>
-            <div className="flex items-center justify-center gap-y-4 flex-col mt-16">
-                <h2 className='text-3xl text-white font-semibold'>
-                    <Translate text='Localization Setup Integrated' />
-                </h2>
-                <Select
-                    defaultValue={localStorage.getItem('locale') ?? 'en'}
-                    className='h-16 w-20'
-                    style={{
-                        width: '200px'
-                    }}
-                    onChange={handleChange}
-                    options={languages}
-                />
-            </div>
+            
 
-            <h2 className='text-3xl text-center text-white font-semibold mt-20'>
-                <Translate text='Libraries used in this stater' />
-            </h2>
-            <div className="grid grid-cols-5 items-center justify-center px-16 gap-4 text-LightGray">
-                <Card title="Ant Design" />
-                <Card title="React Intl" />
-                <Card title="React Query" />
-                <Card title="Axios" />
-                <Card title="Vite-Plugin-SVGR" />
-                <Card title="React-Router-DOM" />
-                <Card title="Lazy Loading" />
-                <Card title="Dayjs" />
-                <Card title="TailwindCSS" />
-                <Card title="Sass" />
-            </div>
+   
+
         </div>
     )
 }
