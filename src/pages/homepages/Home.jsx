72af08e7bbe26658659f3e458311f6f1b37a3d00
Minh Trí Trần <tritranrpc@gmail.com>
2024-11-14 14:39:59 +0700

    change bg

--- src/pages/homepages/Home.jsx
+++ src/pages/homepages/Home.jsx
@@ -2,20 +2,14 @@ import React from 'react'
 import Card from '@components/Card'
 import { useLocalization } from '../../context/LocalizationWrapper';
 import { Select } from 'antd'
-import { languages } from '../../constants/constants';
-import Translate from '../../components/Common/Translate';
+
 
 export default function Home() {
-    const { switchLocale } = useLocalization();
 
-    const handleChange = (value) => {
-        switchLocale(value);
-    };
 
     return (
-        <div className='flex flex-col gap-y-4'>
+        <div className='flex flex-col gap-y-4' >
             
-
    
 
         </div>
