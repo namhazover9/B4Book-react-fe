bf144b9f7c3462abce1fb382e3c135e6ed0d2797
Đăng Hòa Nguyễn <danghoabay7@outlook.com>
2024-11-15 16:10:09 +0700

    feat/B4B-7

--- src/pages/homepages/Home.jsx
+++ src/pages/homepages/Home.jsx
@@ -2,6 +2,11 @@ import React from 'react'
 import Card from '@components/Card'
 import { useLocalization } from '../../context/LocalizationWrapper';
 import { Select } from 'antd'
+import DiscountBanners from './DiscountBanner';
+import TopVendor from './Topvendor'
+import Bestseller from './BestSelling'
+import Test from './test'
+import Trending from './Trending'
 
 
 export default function Home() {
@@ -9,9 +14,11 @@ export default function Home() {
 
     return (
         <div className='flex flex-col gap-y-4' >
-            
-   
-
+            <Test />
+            <DiscountBanners />
+            <TopVendor />
+            <Bestseller />
+            <Trending />
         </div>
     )
 }
