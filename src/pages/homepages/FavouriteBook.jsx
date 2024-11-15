ea9ce8ea0486845fb5e0be9ad34a12967446c5fa
VietDucTN03 <vietductn281103@gmail.com>
2024-11-15 16:33:19 +0700

    fix conflits

--- src/pages/homepages/FavouriteBook.jsx
+++ src/pages/homepages/FavouriteBook.jsx
@@ -1,7 +1,7 @@
 export default function FavouriteBook() {
     return (
         <div className='flex items-center justify-center'>
-            <h1 className='text-6xl text-red-600'>FavouriteBook</h1>
+            <h1 className='text-6xl text-red-600'>Favourite Book</h1>
         </div>
     )
 }
