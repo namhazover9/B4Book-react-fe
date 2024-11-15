bdb062379125854e84e6eeaf54b77e22a2d1fb91
Minh Trí Trần <tritranrpc@gmail.com>
2024-11-15 16:42:18 +0700

    feat:B4B-4

--- src/pages/homepages/Home.jsx
+++ src/pages/homepages/Home.jsx
@@ -10,7 +10,9 @@ export default function Home() {
 
     return (
         <div className='flex flex-col gap-y-4' >
+            <div>
             <BannerSlider/>
+            </div>
             
             
         </div>
