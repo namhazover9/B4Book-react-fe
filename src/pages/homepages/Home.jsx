31a6f35582ba1b47965fcb5e0f947c21c1aff266
cuong215 <cuonghuynh0709@gmail.com>
2024-11-15 16:12:52 +0700

    feat/B4B-7

--- src/pages/homepages/Home.jsx
+++ src/pages/homepages/Home.jsx
@@ -2,15 +2,15 @@ import React from 'react'
 import Card from '@components/Card'
 import { useLocalization } from '../../context/LocalizationWrapper';
 import { Select } from 'antd'
-
+import CategorySlider from './CategorySlider'
 
 export default function Home() {
 
 
     return (
         <div className='flex flex-col gap-y-4' >
-            
-   
+
+            <CategorySlider />
 
         </div>
     )
