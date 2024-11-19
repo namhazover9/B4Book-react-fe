278c039203c5f6c9ee8948fa050f17289bd14185
cuong215 <cuonghuynh0709@gmail.com>
2024-11-19 16:10:02 +0700

    B4B-31/FE - Admin Home

--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -7,6 +7,10 @@ import { lazy } from "react";
 const Home = lazy(() => import("@pages/homepages/Home"));
 const Login = lazy(() => import("@pages/login/Login"));
 const AboutUs = lazy(() => import("@pages/AboutUs/AboutUs"));
+const AdminPage = lazy(() => import("@pages/AdminRole/AdminPage"));
+const Dashboard = lazy(() => import("@pages/AdminRole/DashBoard"));
+const Product = lazy(() => import("@pages/AdminRole/Product"));
+
 
 
 
@@ -27,6 +31,21 @@ export const routes_here = [
     {
         path: "/aboutus",
         element: <AboutUs />,
+        isPrivate: false,
+    },
+    {
+        path: "/admin",
+        element: <AdminPage />,
+        isPrivate: false,
+    },
+    {
+        path: "/dashboard",
+        element: <Dashboard />,
+        isPrivate: false,
+    },
+    {
+        path: "/products",
+        element: <Product />,
         isPrivate: false,
     },
     //routes in objects
