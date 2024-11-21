21987abff7beafc6bd10b6734e1332f7e72c139c
VietDucTN03 <vietductn281103@gmail.com>
2024-11-21 09:19:03 +0700

    update reponsive B4B-6

--- src/pages/homepages/BannerSlider.jsx
+++ src/pages/homepages/BannerSlider.jsx
@@ -36,7 +36,7 @@ export default function BannerSlider() {
     useEffect(() => {
         const interval = setInterval(() => {
             setCurrentSlide((prevSlide) => (prevSlide + 1) % slides.length);
-        }, 10000000);
+        }, 5000);
 
         return () => clearInterval(interval);
     }, [currentSlide]);
@@ -54,72 +54,72 @@ export default function BannerSlider() {
     return (
         <div className="relative w-full h-full mt-36">
             <div className='relative'>
-                <img src="src/assets/images/bannerslider/rev_home6.png" alt="" className='absolute top-0 left-0 z-20' />
+                <img src="src/assets/images/bannerslider/rev_home6.png" alt="" className='absolute w-1/6 sm:w-1/12 top-0 left-0 z-20' />
+                <img src='src/assets/images/bannerslider/rev_home6_1.png' alt="" className='sm:hidden absolute w-1/5 top-80 right-0 z-20' />
             </div>
-            <div className="">
+            <div className="mx-96">
                 {slides.map((slide, index) => (
                     <div
                         key={slide.id}
-                        className={`absolute top-0 left-0 w-full h-5/6 ${currentSlide === index ? 'opacity-100 z-10' : 'opacity-0 z-0'}
-                            flex items-center justify-between ${slide.color} px-20 py-60`}
+                        className={`absolute w-full h-96 top-0 left-0 rep-banner-xl-tnvd sm:flex sm:items-center sm:justify-between ${currentSlide === index ? 'opacity-100 z-10' : 'opacity-0 z-0'} ${slide.color} `}
                         style={{ transition: 'opacity 0.5s ease-in-out' }}
                     >
-                        <div className="slider-left-title text-white w-1/3 text-center">
-                            <div
-                                className={`title-img flex justify-center items-center mb-2 transition-all ${currentSlide === index ? 'opacity-100 translate-y-0' : 'opacity-0 translate-y-12'}`}
-                                style={{ transitionDuration: '1s' }}
-                            >
-                                {slide.decoration && slide.decoration.title ? (
-                                    <img src={slide.decoration.title} alt="Title Decoration" className='w-3/12' />
-                                ) : (
-                                    <span className="text-base">SPECIAL OFFER</span>
-                                )}
-                            </div>
-                            <div
-                                className={`relative transition-all ${currentSlide === index ? 'opacity-100 translate-y-0' : 'opacity-0 translate-y-12'}`}
-                                style={{ transitionDuration: '2s' }}
-                            >
-                                <img src="src/assets/images/bannerslider/revslider_vector.png" alt="" className='absolute top-10 left-0 z-0' />
-                                <h1 className='absolute text-5xl font-semibold mb-5 z-10'>{slide.title}</h1>
-                            </div>
-                            <div
-                                className={`mt-32 transition-all ${currentSlide === index ? 'opacity-100 translate-y-0' : 'opacity-0 translate-y-12'}`}
-                                style={{ transitionDuration: '3s' }}
-                            >
-                                <p>{slide.description}</p>
-                                <button className='group mt-5 bg-white text-black rounded-3xl py-3 px-5 
+                        <div className="slider-left-title text-white w-full h-full sm:w-1/2 text-center flex flex-col justify-center items-center">
+                            <div className="sm:w-3/4 lg:w-3/4 md:w-4/5">
+                                <div
+                                    className={`title-img flex justify-center items-center mb-2 transition-all duration-500 ${currentSlide === index ? 'opacity-100 translate-y-0' : 'opacity-0 translate-y-12'}`}
+                                >
+                                    {slide.decoration && slide.decoration.title ? (
+                                        <img src={slide.decoration.title} alt="Title Decoration" className='w-3/12' />
+                                    ) : (
+                                        <span className="text-base">SPECIAL OFFER</span>
+                                    )}
+                                </div>
+                                <div
+                                    className={`transition-all duration-700 ${currentSlide === index ? 'opacity-100 translate-y-0' : 'opacity-0 translate-y-12'}`}
+                                >
+                                    <div className="relative">
+                                        <img src="src/assets/images/bannerslider/revslider_vector.png" alt="" className='absolute top-7 lg:w-1/2 left-0 z-0' />
+                                        <h1 className='text-2xl sm:text-3xl font-semibold z-10 rep-title-xl-tnvd'>{slide.title}</h1>
+                                    </div>
+                                </div>
+                                <div
+                                    className={`transition-all duration-1000 ${currentSlide === index ? 'opacity-100 translate-y-0' : 'opacity-0 translate-y-12'}`}
+                                >
+                                    <p className='text-sm sm:text-base mt-3'>{slide.description}</p>
+                                    <button className='group mt-5 bg-white text-black rounded-3xl py-3 px-5 
                                     hover:bg-black hover:text-white hover:shadow-lg
                                     transition-all duration-300'>
-                                    SHOP NOW
-                                    <span className='inline-block ml-2 transition-transform transform group-hover:translate-x-2'>
-                                        <RightOutlined />
-                                    </span>
-                                </button>
+                                        SHOP NOW
+                                        <span className='inline-block ml-2 transition-transform transform group-hover:translate-x-2'>
+                                            <RightOutlined />
+                                        </span>
+                                    </button>
+                                </div>
                             </div>
                         </div>
 
-                        <div className="slider-right-image w-1/2">
-                            <div className={`bg-support relative w-full h-full transition-all ${currentSlide === index ? 'opacity-100 scale-100' : 'opacity-0 scale-0'}`}
-                                style={{ transition: 'opacity 1s ease, transform 1s ease' }}
+                        <div className="slider-right-image items-center w-1/2 hidden sm:flex">
+                            <div className={`bg-support relative z-0 w-11/12 flex justify-between items-center rep-bg-support-lg-tnvd rep-bg-support-xl-tnvd rep-bg-support-2xl-tnvd transition-transform duration-1000 bg-opacity-100 ease-in-out ${currentSlide === index ? 'opacity-100 scale-100' : 'opacity-0 scale-0'}`}
                             >
-                                <img src='src/assets/images/bannerslider/rev_home6_1.png' alt="" className='absolute top-0 left-0 w-3/12'/>
-                                <img src='src/assets/images/bannerslider/rev_home6_2.png' alt="" className='absolute bottom-0 right-0 w-2/12'/>
+                                <img src='src/assets/images/bannerslider/rev_home6_1.png' alt="" className='absolute top-0 left-0 w-3/12 2xl:w-1/5' />
+                                <img src='src/assets/images/bannerslider/rev_home6_2.png' alt="" className='absolute bottom-0 right-0 w-2/12' />
                             </div>
-                            <div className={`absolute z-50 w-1/3 flex items-center justify-between ml-28 mt-20 transition-all ${currentSlide === index ? 'scale-100 opacity-100' : 'scale-0 opacity-0'}`} style={{ transition: 'transform 1s ease-out, opacity 1s ease-out' }}>
-                                <img src={slide.decoration.candy} alt="" className='w-3/12'/>
-                                <img src={slide.decoration.giftBox} alt="" className='w-4/12'/>
+                            <div className={`absolute z-40 w-1/3 flex items-center justify-between ml-36 mt-20 sm:w-2/5 sm:ml-12 sm:mt-48 md:ml-14 md:mt-52 lg:ml-28 lg:mt-60 xl:ml-32 xl:mt-72 transition-all ${currentSlide === index ? 'scale-100 opacity-100' : 'scale-0 opacity-0'}`} style={{ transition: 'transform 1s ease-out, opacity 1s ease-out' }}>
+                                <img src={slide.decoration.candy} alt="" className='sm:w-1/4 md:w-1/3 lg:w-1/4 xl:w-3/12 xl:ml-3 2xl:ml-20' />
+                                <img src={slide.decoration.giftBox} alt="" className='sm:w-1/3 xl:w-4/12' />
                             </div>
-                            <div className={`absolute slider-image opacity-100 bottom-10 right-16 w-2/5 flex items-center transition-all ${currentSlide === index ? 'scale-100 opacity-100' : 'scale-0 opacity-0'}`}
+                            <div className={`absolute slider-image opacity-100 bottom-10 right-16 w-2/5 flex items-center xl:justify-center transition-all ${currentSlide === index ? 'scale-100 opacity-100' : 'scale-0 opacity-0'}`}
                                 style={{ transition: 'transform 1s ease-out, opacity 1s ease-out' }}
                             >
-                                <img src={slide.miniBg} alt="" className='mini-bg w-3/4 ml-10' />
-                                <img src={slide.image} alt="" className='absolute w-11/12 h-auto mt-10' />
+                                <img src={slide.miniBg} alt="" className='mini-bg ml-10 rep-minibg-xl-tnvd rep-minibg-2xl-tnvd rep-minibg-lg-tnvd rep-minibg-md-tnvd ' />
+                                <img src={slide.image} alt="" className='absolute rep-imgbook-xl-tnvd rep-imgbook-2xl-tnvd mt-10 rep-imgbook-sm-tnvd' />
                             </div>
                         </div>
                     </div>
                 ))}
             </div>
-            <div className="absolute top-96 left-1/2 z-20 mt-14">
+            <div className="absolute flex justify-center w-full top-80 xl:top-96 z-20">
                 {slides.map((slide, index) => (
                     <button
                         key={slide.id}
