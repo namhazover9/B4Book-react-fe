8eb043e30a831bb2623dc0f9783aff91727ba1f5
vu hoang nam <hoagnam150303@gmail.com>
2024-11-22 09:22:17 +0700

    fix library

--- src/pages/homepages/FavouriteBook.jsx
+++ src/pages/homepages/FavouriteBook.jsx
@@ -1,4 +1,4 @@
-import pic1 from '../../assets/images/BestSelling/32.jpg ';
+import pic1 from '../../assets/images/BestSelling/32.jpg';
 import pic2 from '../../assets/images/BestSelling/33.jpg';
 import pic3 from '../../assets/images/BestSelling/28.jpg';
 import pic4 from '../../assets/images/BestSelling/28.jpg';
