a8ca7e7bc34bf4dac6b46d76bc61565dd937c00c
Minh Trí Trần <tritranrpc@gmail.com>
2024-11-22 15:14:49 +0700

    feat/B4B-4 Update header

--- src/layouts/Layout.jsx
+++ src/layouts/Layout.jsx
@@ -12,37 +12,28 @@ import LogoGallery from '../components/footer/LogoGallery';
 import LogoShopBook from '../components/footer/LogoShopbook';
 import Banner from '../components/footer/Banner';
 import { Button, Select, Tooltip } from 'antd';
-import { Link, useNavigate } from 'react-router-dom';
+import { Link, useNavigate, useLocation } from 'react-router-dom'; // Import useLocation
 import Translate from '../components/Common/Translate';
 import { languages } from '../constants/constants';
 import { useLocalization } from '../context/LocalizationWrapper';
 import LoginPage from '../components/modallogin/Login';
-
 export default function Layout({ children }) {
   const { switchLocale } = useLocalization();
   const [isDropdownOpen, setIsDropdownOpen] = useState(false);
   const [isLoginPopupOpen, setIsLoginPopupOpen] = useState(false);
   const dropdownRef = useRef(null);
   const buttonRef = useRef(null);
-
+  const navigate = useNavigate();
+  const location = useLocation(); // Get the current path
   const handleChange = (value) => {
     switchLocale(value);
   };
-
   const toggleLoginPopup = () => {
     setIsLoginPopupOpen(!isLoginPopupOpen);
   };
-
   const toggleDropdown = () => {
     setIsDropdownOpen((prevState) => !prevState);
   };
-
-  const navigate = useNavigate();
-
-  const handleLogoClick = () => {
-    navigate('/');
-  };
-
   useEffect(() => {
     const handleOutsideClick = (event) => {
       if (
@@ -51,22 +42,26 @@ export default function Layout({ children }) {
         !buttonRef.current.contains(event.target)
       ) {
         setIsDropdownOpen(false);
-        // setIsLoginPopupOpen(false);
       }
     };
-
     document.addEventListener('mousedown', handleOutsideClick);
-
     return () => {
       document.removeEventListener('mousedown', handleOutsideClick);
     };
   }, []);
-
+  const menuItems = [
+    { name: 'Home', path: '/' },
+    { name: 'Shop', path: '/products' },
+    { name: 'Books', path: '/books' },
+    { name: 'Pages', path: '/pages' },
+    { name: 'Blog', path: '/blog' },
+    { name: 'Contact', path: '/contact' },
+  ];
   return (
     <div className='font-cairoRegular'>
       <header className='bg-white shadow-md w-full'>
         {/* Top Row - Navigation Links */}
-        <div className="text-base sm:text-lg md:text-xl lg:text-2xl">
+        <div className='text-base sm:text-lg md:text-xl lg:text-2xl'>
           <div className='container mx-auto flex flex-wrap justify-between px-4 sm:px-6 py-2 text-sm text-gray-600'>
             <div className='flex flex-wrap space-x-4'>
               <Link
@@ -93,145 +88,56 @@ export default function Layout({ children }) {
             </div>
           </div>
         </div>
-
         <div className='w-full h-[0.5px] bg-gray-300'></div>
-
+        {/* Main Navigation Bar */}
         <div className='container mx-auto flex sm:flex-wrap flex-col sm:flex-row items-center justify-between px-4'>
           {/* Left side - Logo and main navigation */}
-          <div
-            className='flex items-center space-x-2 cursor-pointer'
-            onClick={handleLogoClick}>
+          <div className='flex items-center space-x-2 cursor-pointer' onClick={() => navigate('/')}>
             {/* Logo */}
-            <img className='sm:w-28 sm:h-28  w-20 h-20' src='https://res.cloudinary.com/dmyfiyug9/image/upload/v1732094490/logo_b4b_pvldap.png' alt='Logo' />
+            <img
+              className='sm:w-28 sm:h-28 w-20 h-20'
+              src='https://res.cloudinary.com/dmyfiyug9/image/upload/v1732094490/logo_b4b_pvldap.png'
+              alt='Logo'
+            />
             <h2 className='sm:text-4xl text-black font-bold m-0 text-2xl'>BigFour</h2>
           </div>
-
-          {/* Search Bar */}
-          <div className='flex w-full sm:w-1/3 items-center border rounded-full px-2 sm:px-3 py-2 sm:py-3 bg-gray-100'>
-            <input
-              type='text'
-              placeholder='Search products...'
-              className='flex-grow outline-none bg-transparent text-sm sm:text-base text-gray-700 px-2'
-            />
-            <SearchOutlined className='text-white cursor-pointer text-lg sm:text-xl bg-red-500 p-2 rounded-full transition-transform duration-300 transform hover:scale-110' />
-          </div>
-
+          <nav className='w-full flex items-center sm:w-auto flex-wrap justify-between space-x-2 sm:space-x-8 text-sm sm:text-base font-medium'>
+            {menuItems.map((item, index) => (
+              <Link
+                key={index}
+                to={item.path}
+                className={`relative font-bold transition duration-300
+        ${location.pathname === item.path ? 'text-red-500 after:w-full' : 'text-gray-700 after:w-0'}
+        hover:text-red-500
+        after:content-[''] after:block after:h-0.5 after:bg-red-500 after:transition-all after:duration-300`}
+              >
+                <Translate text={item.name} />
+              </Link>
+            ))}
+          </nav>
           {/* Right side - Search bar, icons, and language switch */}
-          <div className='flex space-x-4 mt-4 sm:mt-0 '>
-            {/* Language Selector */}
-            <Select
+          <div className='flex items-center space-x-4 mt-4 mb-4 sm:mt-0 sm:mb-0'>
+          <Select
               className='w-20 sm:w-28'
               defaultValue={localStorage.getItem('locale') ?? 'en'}
               onChange={handleChange}
               options={languages}
             />
-            {/* Icons */}
-            <div className='flex items-center space-x-4 text-gray-700 '>
-              <Button className='text-xs sm:text-sm' onClick={toggleLoginPopup} ref={dropdownRef}>
+            <div className='flex items-center space-x-4 '>
+              <button
+                className='text-xs text-white border-2 bg-red-500 rounded-md px-4 py-2 sm:text-sm sm:text-white sm:border-2 sm:bg-red-500 sm:rounded-md sm:px-4 sm:py-2 hover:bg-red-400 hover:text-white '
+                onClick={toggleLoginPopup}
+                ref={dropdownRef}
+              >
                 Login
-              </Button>
+              </button>
               <Link to='/cart' className='hover:text-red-500'>
-                <ShoppingCartOutlined className='text-xl sm:text-2xl hover:text-red-500' />
+                <ShoppingCartOutlined className='text-xl text-red-400 sm:text-2xl sm:text-red-400 hover:text-white hover:bg-red-500 hover:p-2 p-2 hover:rounded-full' />
               </Link>
             </div>
           </div>
         </div>
-
-        <div className='w-full h-[0.5px] bg-gray-300 sm: mt-4'></div>
-
-        {/* Secondary navigation row */}
-        <div className='flex flex-wrap justify-between px-4 sm:px-10 py-4 bg-gray-50 shadow-md border rounded-lg'>
-          {/* Categories Dropdown */}
-          <div className='relative w-full sm:w-auto mb-4 sm:mb-0 sm:flex' ref={dropdownRef}>
-            <button
-              ref={buttonRef} // Gán ref cho button
-              className='w-full sm:w-auto flex items-center justify-between bg-red-500 text-white font-semibold text-sm sm:text-lg px-4 sm:px-6 py-2 rounded-full shadow hover:bg-red-600 transition duration-300'
-              onClick={toggleDropdown}
-            >
-              <MenuUnfoldOutlined className='text-xl sm:text-2xl mr-2' />
-              <span>
-                <Translate text='Categories' />
-              </span>
-              <DownOutlined className='text-lg sm:text-xl ml-2' />
-            </button>
-
-            {isDropdownOpen && (
-              <div className='absolute left-0 mt-2 w-64 bg-white rounded-lg shadow-lg border border-gray-200 z-50'>
-                {[
-                  { icon: '📖', text: 'Action & Adventure' },
-                  { icon: '⭐', text: 'Americas', active: true },
-                  { icon: '🎨', text: 'Arts & Photography' },
-                  { icon: '📚', text: 'Biographies' },
-                  { icon: '👶', text: "Children's Books" },
-                  { icon: '📜', text: 'Classics' },
-                  { icon: '📝', text: 'Contemporary' },
-                  { icon: '📖', text: 'Education & Reference' },
-                  { icon: '📔', text: 'Genre Fiction' },
-                  { icon: '🏛️', text: 'Historical' },
-                ].map((category, index) => (
-                  <a
-                    key={index}
-                    href='#'
-
-                    className={`flex items-center px-4 py-2 text-sm ${category.active
-                      ? 'text-red-500 font-semibold bg-gray-50'
-                      : 'text-gray-700 hover:bg-gray-100'
-                      } transition duration-300`}
-
-                  >
-                    <span className='mr-2'>{category.icon}</span>
-                    {category.text}
-                  </a>
-                ))}
-              </div>
-            )}
-          </div>
-
-          {/* Main Navigation */}
-          {/* <nav className='flex space-x-8 text-base font-medium'>
-            {['Home', 'Shop', 'Books', 'Pages', 'Blog', 'Contact'].map((item, index) => (
-              <a
-                key={index}
-                href='products'
-                className="relative text-gray-700 font-bold hover:text-red-500 transition duration-300 after:content-[''] after:block after:h-0.5 after:w-0 after:bg-red-500 after:transition-all after:duration-300 hover:after:w-full"
-              >
-                <Translate text={item} />
-              </a>
-            ))}
-          </nav> */}
-
-          <nav className='w-full flex items-center sm:w-auto flex flex-wrap justify-between space-x-2 sm:space-x-8 text-sm sm:text-base font-medium'>
-            {[
-              { name: 'Home', path: '/' },
-              { name: 'Shop', path: 'products' },
-              { name: 'Books', path: '/' },
-              { name: 'Pages', path: '/' },
-              { name: 'Blog', path: '/' },
-              { name: 'Contact', path: '/' },
-            ].map((item, index) => (
-              <a
-                key={index}
-                href={item.path} // Sử dụng đường dẫn của từng mục
-                className="relative text-gray-700 font-bold hover:text-red-500 transition duration-300 after:content-[''] after:block after:h-0.5 after:w-0 after:bg-red-500 after:transition-all after:duration-300 hover:after:w-full"
-              >
-                <Translate text={item.name} />
-              </a>
-            ))}
-          </nav>
-
-          {/* Contact Info */}
-          <div className='xl:flex items-center space-x-4 mt-4 sm:mt-0 hidden sm:block '>
-            <Tooltip title='Call Us' placement='bottom'>
-              <a href='#' className='text-gray-600 hover:text-red-500 transition duration-300'>
-                <PhoneOutlined className='text-lg sm:text-2xl' />
-              </a>
-            </Tooltip>
-            <div className='flex flex-col items-start '>
-              <span className='text-sm sm:text-lg font-semibold'>Contact Us</span>
-              <span className='text-xs sm:text-sm'>Call: 123-456-789</span>
-            </div>
-          </div>
-        </div>
+        <div className='w-full h-[0.5px] bg-gray-300 sm:mb-2'></div>
       </header>
       {/* Login Popup */}
       {isLoginPopupOpen && (
@@ -247,6 +153,7 @@ export default function Layout({ children }) {
           </div>
         </div>
       )}
+      {/* Page Content */}
       {children}
       <LogoShopBook />
       <Banner />
