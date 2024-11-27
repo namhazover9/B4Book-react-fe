bba6cd305601709d7564a7806285db4442051980
Minh Trí Trần <tritranrpc@gmail.com>
2024-11-27 17:00:55 +0700

    B4B-47 Update Product Page API

--- src/hooks/useProductsApi.js
+++ src/hooks/useProductsApi.js
@@ -3,39 +3,20 @@ import { axiosClient } from '../ApiConfig/apiConfig';
 const ACCOUNT_API_ENDPOINT = '/products';
 
 const productsApi = {
-  // Lấy tất cả sản phẩm
-  getAllProducts: () => {
-    const url = ACCOUNT_API_ENDPOINT;
-    return axiosClient.get(url);
-  },
+  // Lấy tất cả sản phẩm với các tham số tìm kiếm
+  getAllProducts: ({ category, minPrice, maxPrice, author, page = 1, limit = 10, sort }) => {
+    // Tạo chuỗi query từ các tham số
+    const queryParams = new URLSearchParams({
+      category,
+      minPrice,
+      maxPrice,
+      author,
+      page,
+      limit,
+      sort,
+    }).toString();
 
-  // Hàm để tạo URL với các bộ lọc nối lại với nhau
-  buildFilterUrl: (filters) => {
-    let url = `${ACCOUNT_API_ENDPOINT}/filter?`;
-    const filterParams = [];
-
-    // Kiểm tra các filter và thêm vào filterParams
-    if (filters.category) {
-      filterParams.push(`category=${filters.category}`);
-    }
-    if (filters.author) {
-      filterParams.push(`author=${filters.author}`);
-    }
-    if (filters.minPrice !== undefined && filters.maxPrice !== undefined) {
-      filterParams.push(`minPrice=${filters.minPrice}&&maxPrice=${filters.maxPrice}`);
-    }
-
-    // Nếu có bộ lọc, nối các tham số vào URL
-    if (filterParams.length > 0) {
-      url += `${filterParams.join('&&')}`;
-    }
-
-    return url;
-  },
-
-  // Lọc sản phẩm theo các điều kiện động
-  getFilteredProducts: (filters) => {
-    const url = productsApi.buildFilterUrl(filters);
+    const url = `${ACCOUNT_API_ENDPOINT}?${queryParams}`;
     return axiosClient.get(url);
   },
 
