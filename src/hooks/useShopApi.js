c58668fc4f8493855065bba77024f65555e46929
vu hoang nam <hoagnam150303@gmail.com>
2024-11-28 09:31:13 +0700

    feat/B4B-51 FE - Shop List Page (User) and fetch API

--- src/hooks/useShopApi.js
+++ src/hooks/useShopApi.js
@@ -4,8 +4,21 @@ const ACCOUNT_API_ENDPOINT = '/shop';
 
 const shopApi = {
   // fn: get all shop
-  getAllShop: () => {
-    const url = ACCOUNT_API_ENDPOINT  ;
+  getAllShop: ({ name, page = 1, limit = 10 }) => {
+    // Tạo chuỗi query từ các tham số
+    const queryParams = new URLSearchParams({
+      name,
+      page,
+      limit,
+    }).toString();
+
+    const url = `${ACCOUNT_API_ENDPOINT}?${queryParams}`;
+    return axiosClient.get(url);
+  },
+
+  // Tìm kiếm sản phẩm theo keyword
+  searchShop: (keyword) => {
+    const url = `/shop/search?keyword=${keyword}`; // Chỉnh lại URL đúng format
     return axiosClient.get(url);
   },
 
