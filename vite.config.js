8a8f331e0c2d3c09c31013ef79a1354fd322faec
Trung Do <trungdo@MacBook-Pro-cua-Trung.local>
2024-12-10 14:27:24 +0700

    update dashboard

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,4 @@
+
 import react from '@vitejs/plugin-react';
 import svgr from 'vite-plugin-svgr';
 
@@ -20,7 +21,7 @@ export default {
   },
   proxy: {
     '/': {
-      target: 'http://localhost:8000',
+      target: process.env.NODE_ENV,
       changeOrigin: true,
     },
   },
