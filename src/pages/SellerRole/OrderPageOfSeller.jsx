7be0cbdbfa9c4e0379dfb3cbd9b571e98397d848
vu hoang nam <hoagnam150303@gmail.com>
2024-12-10 14:59:32 +0700

    test

--- src/pages/SellerRole/OrderPageOfSeller.jsx
+++ src/pages/SellerRole/OrderPageOfSeller.jsx
@@ -25,7 +25,7 @@ export default function OrderPageOfSeller() {
           name: order.customer?.userName || 'Unknown',
           email: order.customer?.email || 'N/A',
           phoneNumber: order.customer?.phoneNumber || 'N/A',
-          status: order.shops?.status || 'Unknown',
+          status: order.shops[0]?.status || 'Unknown',
           totalPrice: `$${order.totalOrderPrice || 0}`,
         })),
       );
