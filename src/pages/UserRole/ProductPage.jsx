fd87d6fb7f5615d02d03d69509928ef57dd17bb2
cuong215 <cuonghuynh0709@gmail.com>
2024-12-12 09:21:35 +0700

    abc

--- src/pages/UserRole/ProductPage.jsx
+++ src/pages/UserRole/ProductPage.jsx
@@ -20,7 +20,7 @@ export default function ProductPage() {
   const [filterBooks, setFilterBooks] = useState([]);
   const [currentPage, setCurrentPage] = useState(1);
   const [totalPages, setTotalPages] = useState(1);
-  const [priceRange, setPriceRange] = useState([0, 200000]);
+  const [priceRange, setPriceRange] = useState([0, 100]);
   const [bookList, setBookList] = useState([]);
   const [booksPerPage, setBooksPerPage] = useState(12); // Số sách mặc định mỗi trang
   const [quantity, setQuantity] = useState(1); // State để lưu số lượng sản phẩm
@@ -325,7 +325,7 @@ export default function ProductPage() {
                   range
                   value={priceRange}
                   onChange={handlePriceRangeChange} // Xử lý khi người dùng thay đổi thanh trượt
-                  max={200000}
+                  max={100}
                   disabled={disabled}
                 />
                 <div className='flex justify-between mt-2'>
@@ -370,15 +370,13 @@ export default function ProductPage() {
           <div className='header-all-books flex justify-between items-center mx-4 my-2 lg:my-0 lg:w-11/12'>
             <div className='option-form-left flex items-center w-1/12 justify-end lg:justify-start'>
               <QrcodeOutlined
-                className={`md:mr-2 choice-icon-tnvd ${
-                  viewMode === 'block' ? 'text-blue-500' : ''
-                }`}
+                className={`md:mr-2 choice-icon-tnvd ${viewMode === 'block' ? 'text-blue-500' : ''
+                  }`}
                 onClick={() => handleViewModeChange('block')}
               />
               <BarsOutlined
-                className={`choice-icon-tnvd hidden md:block ${
-                  viewMode === 'line' ? 'text-blue-500' : ''
-                }`}
+                className={`choice-icon-tnvd hidden md:block ${viewMode === 'line' ? 'text-blue-500' : ''
+                  }`}
                 onClick={() => handleViewModeChange('line')}
               />
             </div>
@@ -466,11 +464,10 @@ export default function ProductPage() {
                                         </Link>
                                       </button>
                                       <button
-                                        className={`flex justify-evenly items-center px-1 py-3 bg-white rounded-full ${
-                                          addingToCart
-                                            ? 'opacity-50 cursor-not-allowed'
-                                            : 'hover:bg-red-500 hover:text-white'
-                                        } transition-all transform translate-x-10 group-hover:translate-x-0 duration-300 delay-150 shadow-lg`}
+                                        className={`flex justify-evenly items-center px-1 py-3 bg-white rounded-full ${addingToCart
+                                          ? 'opacity-50 cursor-not-allowed'
+                                          : 'hover:bg-red-500 hover:text-white'
+                                          } transition-all transform translate-x-10 group-hover:translate-x-0 duration-300 delay-150 shadow-lg`}
                                         onClick={() => handleAddToCart(book._id, quantity)}
                                         disabled={addingToCart} // Vô hiệu hóa khi đang thêm
                                       >
@@ -485,9 +482,8 @@ export default function ProductPage() {
                                 </div>
 
                                 <p
-                                  className={`text-xl font-bold mb-1 text-[#F18966] ${
-                                    isTruncated[index] ?? true ? 'truncate' : ''
-                                  }`}
+                                  className={`text-xl font-bold mb-1 text-[#F18966] ${isTruncated[index] ?? true ? 'truncate' : ''
+                                    }`}
                                   onClick={() => toggleTitle(index)}
                                 >
                                   {book.title}
@@ -501,11 +497,10 @@ export default function ProductPage() {
                                   {[...Array(5)].map((_, index) => (
                                     <span
                                       key={index}
-                                      className={`text-2xl ${
-                                        index < book.ratingResult
-                                          ? 'text-yellow-500'
-                                          : 'text-gray-300'
-                                      }`}
+                                      className={`text-2xl ${index < book.ratingResult
+                                        ? 'text-yellow-500'
+                                        : 'text-gray-300'
+                                        }`}
                                     >
                                       ★
                                     </span>
@@ -586,9 +581,8 @@ export default function ProductPage() {
                                     </p>
                                     <div className='sm:hidden'>
                                       <div
-                                        className={`relative overflow-hidden transition-[max-height] duration-300 ease-in-out ${
-                                          expandedItems[index] ? 'max-h-[500px]' : 'max-h-[80px]'
-                                        }`}
+                                        className={`relative overflow-hidden transition-[max-height] duration-300 ease-in-out ${expandedItems[index] ? 'max-h-[500px]' : 'max-h-[80px]'
+                                          }`}
                                       >
                                         <p className='text-gray-600 text-sm mr-2 text-balance'>
                                           {book.description}
